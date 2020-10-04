87a9feae1e0d1f88f269597c0532fcf72fa24114
Pete Ownby <pete.ownby@gmail.com>
2020-10-04 16:13:54 -0600

    Moved About section to assets

--- src/views/About.js
+++ src/views/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './less/About.less';
+import Projects from '../../assets/projects.json';
 
 function About() {
   return (
@@ -11,11 +12,7 @@ function About() {
       </div>
 
       <div className="content-bar">
-        <div className="content">
-          <p>Pete is a software developer who specializes in Web Development, particularly in front end solutions. He has most of his experience in C# .NET systems, MS SQL, and a variety of front end technologies including Angular, AngularJS, Bootstrap, jQuery, HTML5, CSS3, Typescript, and plain old ES5. He enjoys learning new technologies and solving challenging issues.</p>
-          <p>Pete has graduate and undergraduate degrees in Computer Science and finds it delightful when he can apply concepts and algorithms learned from those programs into applications. He is passionate about writing efficient and performant solutions. He takes pride in his work and expects his releases to have minimal bugs.</p>
-          <p>Outside of work, Pete enjoys relaxing with quiet time, although hiking and traveling are also welcome activities. He has traveled across a number of states and continents, but has many more to visit.</p>
-        </div>
+        <div className="content" dangerouslySetInnerHTML={{ __html: Projects.about }}></div>
       </div>
     </>
   )
