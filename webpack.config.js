e0c0576b59eb4a18d7feff306399205cfd5c4cbe
pownby <pete.ownby@gmail.com>
2025-01-27 15:22:10 -0700

    Update source map scheme

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,44 @@
 const path = require('path');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/main.js'),
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/assets/',
-  },
-  mode: 'production',
-  devtool: 'sourcemap',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.less$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'less-loader']
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        exclude: /node_modules/,
-        use: ['file-loader']
-      }
-    ]
-  },
-  devServer: {
-    publicPath: '/dist',
-    historyApiFallback: true
+module.exports = (env, argv) => {
+  const devMode = argv.mode === 'development';
+
+  return {
+    entry: path.resolve(__dirname, 'src/main.js'),
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/assets/',
+    },
+    mode: 'production',
+    devtool: devMode && 'cheap-module-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.less$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader', 'less-loader']
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          exclude: /node_modules/,
+          use: ['file-loader']
+        }
+      ]
+    },
+    devServer: {
+      publicPath: '/dist',
+      historyApiFallback: true
+    }
   }
 }
